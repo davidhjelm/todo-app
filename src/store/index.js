955534145d23e74116c20168b379a10d2a2e11d3
davidhjelm <davidhjelm@fastmail.com>
2019-04-02 11:48:10 +0200

    set up vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,12 @@
+import todo from './modules/todo'
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+  modules: {
+    todo
   }
 })
